731a3295ccc95b73c35856400b3e0a4b9993bf6b
sangeeta <sangeetabhatia03@gmail.com>
2022-08-09 19:27:49 +0100

    S4 mix fitted to transmission pairs

---
+++ src/process_isol_gamma_alietal_pairs/process_gamma_fit.R
@@ -0,0 +1,21 @@
+## orderly::orderly_develop_start(use_draft = "newer")
+source("utils.R")
+fit <- readRDS("isol_gamma_alietal.rds")
+tab1 <- fitted_params(fit)
+samples_tost <- estimated_TOST_gamma(
+  tab1, n = 1e4, extract(fit)
+)
+summary_tost <- tidy(summary(samples_tost[[1]]))
+
+obs_data <- readRDS("alietal_data_clean.rds")
+common_params <- readRDS("common_params.rds")
+
+best_si <- estimated_SI(
+  obs_data, samples_tost$TOST_bestpars,
+  mixture = TRUE, isol = TRUE,
+  tab1 = tab1, shape_inc = common_params$param_inc$shape,
+  scale_inc = common_params$param_inc$scale
+)
+
+saveRDS(best_si, "best_si.rds")
+saveRDS(summary_tost, "summary_tost.rds")
