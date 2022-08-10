f57dd91fc8f4a8d9691d4591bcaff9d18b5c7748
sangeeta <sangeetabhatia03@gmail.com>
2022-08-10 17:05:47 +0100

    S3 mixture, gamma, pairs

--- src/fit_no_isol_gamma_alietal_pairs/script.R
+++ src/fit_no_isol_gamma_alietal_pairs/script.R
@@ -1,30 +1,25 @@
 ##orderly::orderly_develop_start()
-cowling <- readRDS("cowling_data_clean.rds")
-cowling$si <- as.numeric(cowling$si, units="days")
-## Incubation period distribution
-## https://pubmed.ncbi.nlm.nih.gov/32150748/
-param_inc <- list(
-  shape = 5.807, scale = 0.948
-)
-
-si_vec <- seq(-20, 40, 1)
-
+alietal <- readRDS("alietal_data_clean.rds")
+alietal$si <- as.numeric(alietal$si, units="days")
+common_params <- readRDS("common_params.rds")
 fit <- stan(
   file = "scenario3a_mixture_gamma.stan",
   data = list(
-    N = length(cowling$si),
-    si = cowling$si,
-    max_shed = 21,
-    offset = 3,
-    alpha2 = param_inc$shape,
-    beta2 = param_inc$scale,
-    max_invalid_si = 40,
-    min_invalid_si = -20,
-    width = 0.5,
-    M = length(si_vec),
-    si_vec = si_vec
+    N = length(alietal$si),
+    si = alietal$si,
+    max_shed = common_params$max_shed,
+    offset = common_params$offset,
+    alpha2 = common_params$param_inc$shape,
+    beta2 = common_params$param_inc$scale,
+    max_invalid_si = common_params$max_invalid_si,
+    min_invalid_si = common_params$min_invalid_si,
+    width = common_params$width,
+    M = length(common_params$si_vec),
+    si_vec = common_params$si_vec
   ),
-  seed = 123,
-  chains = 2,
+  seed = common_params$seed,
+  chains = common_params$chains, iter = common_params$iter,
   verbose = TRUE
 )
+
+saveRDS(fit, "no_isol_gamma_alietal_pairs.rds")
--- workflow.R
+++ workflow.R
@@ -1,7 +1,11 @@
 library(orderly)
 
+orderly_run('fit_no_isol_gamma_alietal_pairs', use_draft = 'newer')
+
+
+
 orderly_run('process_isol_sn_alietal_pairs', use_draft = 'newer')
-orderly_run('plot_isol_sn_alietal_pairs', use_draft = 'newer')
+Orderly_run('plot_isol_sn_alietal_pairs', use_draft = 'newer')
 
 orderly_run('process_isol_phen_alietal_pairs', use_draft = 'newer')
 orderly_run('plot_isol_phen_alietal_pairs', use_draft = 'newer')
