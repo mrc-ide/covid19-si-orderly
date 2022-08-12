deb04ed0050e18f20c855f0fca2d458e7b288798
sangeeta <sangeetabhatia03@gmail.com>
2022-08-12 14:14:39 +0100

    Part isolation, gamma

--- workflow.R
+++ workflow.R
@@ -3,12 +3,11 @@ use_draft <- 'newer'
 
 orderly_run('fit_no_isol_gamma_alietal_pairs', use_draft = use_draft)
 
-
 orderly_run('process_no_isol_gamma_alietal_pairs', use_draft = use_draft)
 orderly_run('plot_no_isol_gamma_alietal_pairs', use_draft = use_draft)
 
 orderly_run('process_isol_sn_alietal_pairs', use_draft = use_draft)
-Orderly_run('plot_isol_sn_alietal_pairs', use_draft = use_draft)
+orderly_run('plot_isol_sn_alietal_pairs', use_draft = use_draft)
 
 orderly_run('process_isol_phen_alietal_pairs', use_draft = use_draft)
 orderly_run('plot_isol_phen_alietal_pairs', use_draft = use_draft)
@@ -16,3 +15,6 @@ orderly_run('plot_isol_phen_alietal_pairs', use_draft = use_draft)
 
 orderly_run('process_isol_gamma_alietal_pairs', use_draft = use_draft)
 orderly_run('plot_isol_gamma_alietal_pairs', use_draft = use_draft)
+
+orderly_run('process_part_isol_gamma_alietal', use_draft = use_draft)
+orderly_run('plot_part_isol_gamma_alietal', use_draft = use_draft)
