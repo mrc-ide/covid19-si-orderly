529db96381f6e7ac094aa3859706f50f7eced4c3
sangeeta <sangeetabhatia03@gmail.com>
2022-08-19 09:41:17 +0100

    Gamma, part isolation, pairs only

---
+++ src/plot_part_isol_gamma_alietal_pairs/script.R
@@ -0,0 +1,38 @@
+## orderly::orderly_develop_start(use_draft = "newer")
+source("utils_figs.R")
+obs_data <- readRDS("alietal_data_clean.rds")
+best_si <- readRDS("best_si.rds")
+
+
+summary_tab <- data.frame(
+  SI = c("True       ", "Expected"),
+  Mean = c(mean(best_si$unconditional),
+           mean(best_si$mixed_c)),
+  SD = c(sd(best_si$unconditional),
+         sd(best_si$mixed_c))
+)
+
+
+summary_tab <- mutate_if(
+  summary_tab, is.numeric,
+  make_pretty
+)
+
+dfnpc <- tibble(x = 0.95, y = 0.95, tb = list(summary_tab))
+
+
+p <- plot_both_SIs(
+  best_si$unconditional,
+  best_si$mixed_c,
+  obs_data
+)
+
+
+p <- p +
+  geom_table_npc(
+    data = dfnpc, aes(npcx = x, npcy = y, label = tb),
+    table.theme = ttheme_gtminimal()
+  ) + ggtitle("Estimated using isolation for some pairs, fitted using discrete pairs",
+              subtitle = "Infectious period assumed to be gamma distributed")
+
+ggsave("part_isol_gamma_alietal_pairs.png", p)
